3fea47985979d612b22889b3b20bf68c6a13aa4e
ThinhTran <thinhtran3803@gmail.com>
2023-12-26 17:32:16 +0700

    detect finger

--- Application.py
+++ Application.py
@@ -1,30 +1,18 @@
 import sys
-
-sys.path.append('/home/thinhtran/smartlock')
-
 from datetime import datetime
 from modules import I2C_LCD_driver
 from modules.keypad_module import Keypad
 from modules.led_module import LEDController
 from modules.Fingerprint import FingerPrint
-
 import time
-
 
 row_pins = [17, 27, 22, 5]
 col_pins = [23, 24, 25, 16]
 
 lcd = I2C_LCD_driver.lcd()
 finger = FingerPrint(lcd)
-
-
-
 led = LEDController()
-
-
 keypad = Keypad(row_pins, col_pins)
-
-
 
 waitingForInput = False
 enteringPassword = False
@@ -32,10 +20,7 @@ rightPassword = False
 
 buffer = ''
 password = '3897'
-keyInput = '';
-
-
-
+keyInput = ''
 
 while True:
     key = str(keypad.get_key())
@@ -54,11 +39,10 @@ while True:
             lcd.lcd_clear()
             if password == buffer[1:]:
                 lcd.lcd_display_string('Unlock success', 1, 0)
-                
                 led.ledOn()
                 time.sleep(3)
                 led.ledOff()
-                lcd.lcd_display_string('Input password:',1, 0)
+                lcd.lcd_display_string('Input password:', 1, 0)
                 rightPassword = True
             else:
                 lcd.lcd_display_string('Password wrong', 1, 0)
@@ -68,17 +52,13 @@ while True:
         elif key == 'C' and waitingForInput:
             if buffer[1:] == password:
                 finger.enrollFinger()
-        
-        elif key == 'None':
+    elif key == 'None':
+        if not waitingForInput:  # Only detect finger when not waiting for input
             finger.detectFinger()
-                
-            
-                
+
     if waitingForInput and key != 'None':
-            keyInput += '*'
-            lcd.lcd_display_string(keyInput[1:],2,0)
-            buffer += key
-            print('Buffer: ' + buffer)
+        keyInput += '*'
+        lcd.lcd_display_string(keyInput[1:], 2, 0)
+        buffer += key
+        print('Buffer: ' + buffer)
     time.sleep(0.1)
-    
- 
