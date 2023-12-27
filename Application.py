44ee1b0dfecaea7224f8ff8e98a4f82ce98e5a63
ThinhTran <thinhtran3803@gmail.com>
2023-12-27 12:12:02 +0700

    refactor to threading

--- Application.py
+++ Application.py
@@ -15,7 +15,7 @@ finger = FingerPrint(lcd)
 led = LEDController()
 keypad = Keypad(row_pins, col_pins)
 
-waitingForInput = False
+waitingForInput = True
 enteringPassword = False
 rightPassword = False
 
@@ -23,48 +23,58 @@ buffer = ''
 password = '3897'
 keyInput = ''
 
-def fingerprint_thread():
+def fingerPrintThread():
     while True:
         finger.detectFinger()
         
 
-finger_thread = threading.Thread(target=fingerprint_thread, daemon=True)
+finger_thread = threading.Thread(target=fingerPrintThread, daemon=True)
 finger_thread.start()
 
+def passcodeThread():
+    while True:
+        key = str(keypad.get_key())
+        lcd.lcd_display_string('Input password: ', 1, 0)
+        if waitingForInput and key != 'None':
+            keyInput += '*'
+            lcd.lcd_display_string(keyInput[1:], 2, 0)
+            buffer += key
+            print('Buffer: ' + buffer)
+
+passcode_thread = threading.Thread(target=passcodeThread, daemon=True)
 
 
+# while True:
+#     key = str(keypad.get_key())
 
-while True:
-    key = str(keypad.get_key())
-
-    if key != 'None':
-        if key == 'A':
-            lcd.lcd_clear()
-            lcd.lcd_display_string('Input password:')
-            waitingForInput = True
-            enteringPassword = True
-            buffer = ''
-            keyInput = ''
-        elif key == 'B' and waitingForInput:
-            waitingForInput = False
-            lcd.lcd_clear()
-            if password == buffer[1:]:
-                lcd.lcd_display_string('Unlock success', 1, 0)
-                led.ledOn()
-                time.sleep(3)
-                led.ledOff()
-                lcd.lcd_display_string('Input password:', 1, 0)
-                rightPassword = True
-            else:
-                lcd.lcd_display_string('Password wrong', 1, 0)
-                time.sleep(3)
-                lcd.lcd_display_string('Input password: ', 1, 0)
+#     if key != 'None':
+#         if key == 'A':
+#             lcd.lcd_clear()
+#             lcd.lcd_display_string('Input password:')
+#             waitingForInput = True
+#             enteringPassword = True
+#             buffer = ''
+#             keyInput = ''
+#         elif key == 'B' and waitingForInput:
+#             waitingForInput = False
+#             lcd.lcd_clear()
+#             if password == buffer[1:]:
+#                 lcd.lcd_display_string('Unlock success', 1, 0)
+#                 led.ledOn()
+#                 time.sleep(3)
+#                 led.ledOff()
+#                 lcd.lcd_display_string('Input password:', 1, 0)
+#                 rightPassword = True
+#             else:
+#                 lcd.lcd_display_string('Password wrong', 1, 0)
+#                 time.sleep(3)
+#                 lcd.lcd_display_string('Input password: ', 1, 0)
             
                 
 
-    if waitingForInput and key != 'None':
-        keyInput += '*'
-        lcd.lcd_display_string(keyInput[1:], 2, 0)
-        buffer += key
-        print('Buffer: ' + buffer)
-    time.sleep(0.1)
+#     if waitingForInput and key != 'None':
+#         keyInput += '*'
+#         lcd.lcd_display_string(keyInput[1:], 2, 0)
+#         buffer += key
+#         print('Buffer: ' + buffer)
+#     time.sleep(0.1)
