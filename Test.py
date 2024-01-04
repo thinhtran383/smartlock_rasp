27ad126095ec54de1be06a16ff6039ad17fee4e9
ThinhTran <thinhtran3803@gmail.com>
2024-01-04 23:55:05 +0700

    fixgp

--- Test.py
+++ Test.py
@@ -8,7 +8,6 @@ from datetime import datetime
 import threading
 import time
 
-
 rowPins = [17, 27, 22, 5]
 colPins = [23, 24, 25, 16]
 
@@ -24,33 +23,27 @@ keyInput = ''
 buffer = ''
 rootPassword = '3897'
 
-
-
 condition = threading.Condition()
 lcdLock = threading.Lock()
-
+fingerprintLock = finger.fingerprint_lock
 
 def currentDateTime():
     currentTime = datetime.now()
-    
+
     dateFormat = '%d-%m-%y'
     timeFormat = '%H:%M:%S'
-    
+
     formattedDate = currentTime.strftime(dateFormat)
     formattedTime = currentTime.strftime(timeFormat)
-    
+
     return formattedDate, formattedTime
-        
-        
 
 def passcode():
     key = str(keypad.get_key())
     return key if key != 'None' else None
-        
-        
+
 def checkPasscode(passcode):
     return passcode == rootPassword
-        
 
 def fingerDetect():
     global pauseThread
@@ -58,51 +51,69 @@ def fingerDetect():
     global buffer
     global keyInput
 
-    
-    
     while True:
         with condition:
             while pauseThread:
                 condition.wait()
-            status = finger.detectFinger()
-                
+
+            with fingerprintLock:
+                status = finger.detectFinger()
+
             if status == 0:
                 with lcdLock:
                     lcd.lcd_clear()
                     lcd.lcd_display_string('Cannot detect', 1, 0)
                     lcd.lcd_display_string('finger', 2, 0)
                     time.sleep(1.5)
-                    
+
             if status == 1:
                 with lcdLock:
                     lcd.lcd_clear()
                     lcd.lcd_display_string('Unlock success', 1, 0)
                     time.sleep(1.5)
-                    
-                    
-                    
+
+            if keyInput.endswith('#B'):
+                # Nếu người dùng nhập đúng mật khẩu root và nhấn 'B'
+                with fingerprintLock:
+                    result = finger.enrollFinger()
+
+                    if result == 3:
+                        with lcdLock:
+                            lcd.lcd_clear()
+                            lcd.lcd_display_string('Finger enrolled!', 1, 0)
+                            time.sleep(1.5)
+                            lcd.lcd_clear()
+                            keyInput = ''
+                            buffer = ''
+                            showDatetime = True
+                    elif result == 2:
+                        with lcdLock:
+                            lcd.lcd_clear()
+                            lcd.lcd_display_string('Enrollment failed', 1, 0)
+                            time.sleep(1.5)
+                            lcd.lcd_clear()
+                            keyInput = ''
+                            buffer = ''
+                            showDatetime = True
+
             showDatetime = True
             buffer = ''
             keyInput = ''
-            
-
 
 if __name__ == '__main__':
     fingerThread = threading.Thread(target=fingerDetect)
     fingerThread.start()
-   
+
     try:
-        while True:   
+        while True:
             pressedKey = passcode()
-            
-            
-            
+
             if showDatetime:
                 date, currentTime = currentDateTime()
                 with lcdLock:
-                    lcd.lcd_display_string('Date: '+ date, 1, 0)
+                    lcd.lcd_display_string('Date: ' + date, 1, 0)
                     lcd.lcd_display_string('Time: ' + currentTime, 2, 0)
-            
+
             if pressedKey is not None:
                 showDatetime = False
                 keyInput += pressedKey
@@ -110,17 +121,17 @@ if __name__ == '__main__':
                 print('keyInput: ' + keyInput)
                 with lcdLock:
                     lcd.lcd_clear()
-                    lcd.lcd_display_string('Input passcode:' , 1, 0)
+                    lcd.lcd_display_string('Input passcode:', 1, 0)
                     lcd.lcd_display_string(buffer, 2, 0)
-                
-                if keyInput[-1] == 'B': # so sanh ky tu cuoi cung
-                    if checkPasscode(keyInput[:-1]):    
+
+                if keyInput[-1] == 'B':
+                    if checkPasscode(keyInput[:-1]):
                         lcd.lcd_clear()
                         lcd.lcd_display_string('Unlock success', 1, 0)
                         time.sleep(1.5)
                         lcd.lcd_clear()
-                        
-                        keyInput =''
+
+                        keyInput = ''
                         buffer = ''
                         showDatetime = True
                     else:
@@ -128,27 +139,24 @@ if __name__ == '__main__':
                         lcd.lcd_display_string('Passcode wrong!!!', 1, 0)
                         time.sleep(1.5)
                         lcd.lcd_clear()
-                        
+
                         keyInput = ''
                         buffer = ''
                         showDatetime = True
-                        
-                        
-                
+
             if pressedKey == '*' and pressedKey is not None:
                 pauseThread = True
-            
+
             if pressedKey == '#' and pressedKey is not None:
                 pauseThread = False
                 with condition:
                     condition.notify_all()
-                
+
             if pressedKey == 'C' and pressedKey is not None:
                 keyInput = ''
                 buffer = ''
                 lcd.lcd_clear()
-                lcd.lcd_display_string('Input passcode: ',1,0)
-            
-            
+                lcd.lcd_display_string('Input passcode: ', 1, 0)
+
     except KeyboardInterrupt:
         fingerThread.join()
--- modules/Fingerprint.py
+++ modules/Fingerprint.py
@@ -1,7 +1,4 @@
 import sys
-
-sys.path.append('/home/thinhtran/smartlock/modules')
-
 import time
 from threading import Lock
 from as608_driver import PyFingerprint, FINGERPRINT_CHARBUFFER1, FINGERPRINT_CHARBUFFER2
@@ -24,112 +21,105 @@ class FingerPrint:
                 print('The fingerprint sensor could not be initialized!')
                 print('Exception message: ' + str(e))
                 sys.exit(1)
+            cls._instance.fingerprint_lock = Lock()
         return cls._instance
 
     def __init__(self, port='/dev/ttyS0', baudrate=57600, password=0xFFFFFFFF, address=0x00000000):
-        pass  
+        pass
 
     def enrollFinger(self):
         try:
             print('Waiting for finger...')
-            
-            while not self.fingerprint.readImage():
-                pass
+            with self.fingerprint_lock:
+                while not self.fingerprint.readImage():
+                    pass
 
-            self.fingerprint.convertImage(FINGERPRINT_CHARBUFFER1)
+                self.fingerprint.convertImage(FINGERPRINT_CHARBUFFER1)
 
-            result = self.fingerprint.searchTemplate()
-            positionNumber = result[0]
+                result = self.fingerprint.searchTemplate()
+                positionNumber = result[0]
 
-            if positionNumber >= 0:
-                print('Template already exists at position #' + str(positionNumber))
-                return 1
+                if positionNumber >= 0:
+                    print('Template already exists at position #' + str(positionNumber))
+                    return 1
 
-            print('Remove finger...')
-            time.sleep(2)
+                print('Remove finger...')
+                time.sleep(2)
 
-            print('Waiting for the same finger again...')
-            
-            while not self.fingerprint.readImage():
-                pass
+                print('Waiting for the same finger again...')
+                while not self.fingerprint.readImage():
+                    pass
 
-            self.fingerprint.convertImage(FINGERPRINT_CHARBUFFER2)
+                self.fingerprint.convertImage(FINGERPRINT_CHARBUFFER2)
 
-            if self.fingerprint.compareCharacteristics() == 0:
-                print('Fingers do not match')
-                return 2
+                if self.fingerprint.compareCharacteristics() == 0:
+                    print('Fingers do not match')
+                    return 2
 
-            self.fingerprint.createTemplate()
+                self.fingerprint.createTemplate()
 
-            positionNumber = self.fingerprint.storeTemplate()
-            print('Finger enrolled successfully!')
-            print('New template position #' + str(positionNumber))
-            
-            self.fingerprint.loadTemplate(positionNumber, FINGERPRINT_CHARBUFFER1)
-            characteristics = str(self.fingerprint.downloadCharacteristics(FINGERPRINT_CHARBUFFER1)).encode('utf-8')
-            print('SHA-2: ' + hashlib.sha256(characteristics).hexdigest())
-            return 3
+                positionNumber = self.fingerprint.storeTemplate()
+                print('Finger enrolled successfully!')
+                print('New template position #' + str(positionNumber))
+
+                self.fingerprint.loadTemplate(positionNumber, FINGERPRINT_CHARBUFFER1)
+                characteristics = str(self.fingerprint.downloadCharacteristics(FINGERPRINT_CHARBUFFER1)).encode('utf-8')
+                print('SHA-2: ' + hashlib.sha256(characteristics).hexdigest())
+                return 3
         except Exception as e:
             print('Operation failed!')
             print('Exception message: ' + str(e))
             return 2
 
-    def detectFinger(self):  
-        
+    def detectFinger(self):
         try:
             print('Waiting for finger...')
-            while not self.fingerprint.readImage():
-                pass
+            with self.fingerprint_lock:
+                while not self.fingerprint.readImage():
+                    pass
 
-            self.fingerprint.convertImage(FINGERPRINT_CHARBUFFER1)
+                self.fingerprint.convertImage(FINGERPRINT_CHARBUFFER1)
 
-            result = self.fingerprint.searchTemplate()
+                result = self.fingerprint.searchTemplate()
+                positionNumber = result[0]
+                accuracyScore = result[1]
 
-            positionNumber = result[0]
-            accuracyScore = result[1]
-            
-            
-            
+                if positionNumber == -1:
+                    print('Match not found!')
+                    return 0
+                else:
+                    print('Found template at position #' + str(positionNumber))
+                    print('The accuracyScore is: ' + str(accuracyScore))
 
-            if positionNumber == -1:
-                print('Match not found!')
-                return 0
-            else:
-                print('Found template at position #' + str(positionNumber))
-                print('The accuracyScore is: ' + str(accuracyScore))
-                
+                self.fingerprint.loadTemplate(positionNumber, FINGERPRINT_CHARBUFFER1)
 
-            self.fingerprint.loadTemplate(positionNumber, FINGERPRINT_CHARBUFFER1)
+                characteristics = str(self.fingerprint.downloadCharacteristics(FINGERPRINT_CHARBUFFER1)).encode('utf-8')
+                print('SHA-2: ' + hashlib.sha256(characteristics).hexdigest())
 
-            characteristics = str(self.fingerprint.downloadCharacteristics(FINGERPRINT_CHARBUFFER1)).encode('utf-8')
-            print('SHA-2: ' + hashlib.sha256(characteristics).hexdigest())
-
-            return 1
+                return 1
         except Exception as e:
             print('Operation failed')
             print('Exception message: ' + str(e))
             return 0
-    
+
     def deleteFinger(self):
         try:
             positionNumber = input('Please enter the template position you want to delete: ')
             positionNumber = int(positionNumber)
 
-            if self.fingerprint.deleteTemplate(positionNumber):
-                print('Template deleted')
+            with self.fingerprint_lock:
+                if self.fingerprint.deleteTemplate(positionNumber):
+                    print('Template deleted')
         except Exception as e:
             print('Exception message: ' + str(e))
-            
+
     def stop(self):
         sys.exit(1)
-    
+
     def deleteAllTemplate(self):
-        self.fingerprint.clearDatabase()
-        
-    
-        
+        with self.fingerprint_lock:
+            self.fingerprint.clearDatabase()
 
 # Example usage:
-#finger = FingerPrint()
-#print(finger.enrollFinger())
-
+# finger = FingerPrint()
+# print(finger.enrollFinger())
